22b364c6962d0757d783fedba174f20f91b6f99e
Tobiasz Cieplinski <cieplinski.t@gmail.com>
2017-12-20 13:51:16 +0100

    Query, db name and dump cap from system argvs. Credentials moved to separate file.

--- src/crawler.py
+++ src/crawler.py
@@ -2,23 +2,24 @@ import spotipy
 import pprint
 import requests
 import datetime
+import credentials
+import sys
 
 from spotipy.client import SpotifyException
 from tinydb import TinyDB
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_cred = SpotifyClientCredentials(
-    client_id='deae86cc8621462cbd5e1194c06b93ef',
-    client_secret='1a6dd36a26d642879c6e36d0ac0ebb87'
-)
-
-query = 'a*'
+query = sys.argv[1]
+database_name = sys.argv[2]
+dump_cap = sys.argv[3]
 max_limit = 50
 status_ok = 200
-dump_cap = 100
 playlists = []
-database_name = 'database.json'
 
+client_cred = SpotifyClientCredentials(
+    client_id = credentials.client_creds['client_id'],
+    client_secret = credentials.client_creds['client_secret']
+)
 db = TinyDB(database_name)
 api = spotipy.Spotify(client_credentials_manager = client_cred)
 results = api.search(q=query,limit=max_limit, type='playlist')
